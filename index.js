1b128bdc643260609547fa47cb032bb52af40dfc
mkalidas-prdxn <71173672+mkalidas-prdxn@users.noreply.github.com>
2020-11-19 19:09:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express');
 var path = require('path');
+var Busboy = require('busboy');
+var fs = require('fs');
 
 const app = express();
 
 app.get('/', function(req,res){
   res.sendFile(path.join(__dirname + '/home.html'));
 })
+
+app.post('/fileupload', function (req, res) {
+  var busboy = new Busboy({ headers: req.headers });
+  busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    var saveTo = path.join(__dirname,filename);
+    file.pipe(fs.createWriteStream(saveTo));
+  });
+
+  busboy.on('finish', function() {
+    res.writeHead(200, { 'Connection': 'close' });
+    res.end("file saved");
+  });
+  return req.pipe(busboy);    
+});
 
 app.listen(8080,function(req, res){
   console.log('server running');
